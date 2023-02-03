14b9a3077cf2084da46d8db770abdc3331ddb257
Dana Bradley <dbradley0909@outlook.com>
2023-02-03 09:23:48 -0500

    first commit

---
+++ atm.js
@@ -0,0 +1,7 @@
+let displayScreen=[];
+const addToScreen=(btn1)=>{
+    let showScreen = document.querySelector('.btn-number')
+    showScreen.append()
+    displayScreen.push(btn1);
+    console.log(displayScreen);
+}
